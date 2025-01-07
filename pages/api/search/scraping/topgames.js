c08da106b74885aea2a9c674046a15462c84bcc7
unknown <peyonmanuel@gmail.com>
2025-01-07 11:48:33 +0100

    testing playwright

--- pages/api/search/scraping/topgames.js
+++ pages/api/search/scraping/topgames.js
@@ -3,10 +3,6 @@ export default async (req, res) => {
     query: { page },
   } = req;
 
-  // Log page query to verify input
-  console.log("Page:", page);
-
-  // Check if the page query parameter exists
   if (!page) {
     return res.status(400).json({ error: "Page parameter is required" });
   }
@@ -18,12 +14,15 @@ export default async (req, res) => {
   let hasNextPage = false;
 
   try {
-    const playwright = require("playwright");
+    const playwright = require("playwright-core");
+    const chrome = require("chrome-aws-lambda");
 
-    // Launch the browser
+    // Launch the browser with correct options
     browser = await playwright.chromium.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+      ignoreHTTPSErrors: true,
     });
 
     // Create a new browser context and page
@@ -32,8 +31,7 @@ export default async (req, res) => {
 
     // Navigate to the URL
     const url = `https://www.backloggd.com/games/lib/popular?page=${page}`;
-    console.log(`Navigating to: ${url}`);
-    await pageInstance.goto(url); // 30 seconds timeout
+    await pageInstance.goto(url, { timeout: 30000 });
 
     // Extract titles and next page status
     const result = await pageInstance.evaluate(() => {
@@ -60,21 +58,14 @@ export default async (req, res) => {
     titles = result.titles;
     hasNextPage = result.hasNextPage;
 
-    // Log the result before returning it
-    console.log("Scraped data:", { titles, hasNextPage });
-
     // Send the scraped data as a JSON response
     res.status(200).json({ results: titles, hasNextPage });
   } catch (error) {
-    // Handle any errors
     console.error("Error:", error.message);
-
-    // Return a 500 error with details
     res
       .status(500)
       .json({ error: "Error during scraping", details: error.message });
   } finally {
-    // Clean up and close the browser
     if (browser) {
       await browser.close();
     }
